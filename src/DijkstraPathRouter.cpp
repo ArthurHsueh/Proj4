3f42f60bd6088e625f6e034cc9cc0abe038c736c
root <ajhsueh@ucdavis.edu>
2026-03-10 23:13:48 +0000

    Implemented functions for DijkstraPathRouter.cpp and added tests for DijkstraPathRouterTest.cpp

--- src/DijkstraPathRouter.cpp
+++ src/DijkstraPathRouter.cpp
@@ -1,4 +1,6 @@
 #include "DijkstraPathRouter.h"
+#include <queue>
+#include <algorithm>
 
 struct CDijkstraPathRouter::SImplementation{
     struct SVertex;
@@ -12,36 +14,37 @@ struct CDijkstraPathRouter::SImplementation{
     SImplementation(){
 
     }
+
     ~SImplementation(){
 
     }
 
     std::size_t VertexCount() const noexcept{
-        return DVertices.size();
+        return DVertices.size(); //Return the size of the vector storing the vertices
     }
 
     TVertexID AddVertex(std::any tag) noexcept{
-        auto NewVertex = std::make_shared<SVertex>();
-        NewVertex->DTag = tag;
-        TVertexID NewID = DVertices.size();
-        DVertices.push_back(NewVertex);
+        auto NewVertex = std::make_shared<SVertex>(); //Create a shared pointer to an SVertex
+        NewVertex->DTag = tag; //Assings tag to the vertex 
+        TVertexID NewID = DVertices.size(); //Assigns the ID (index of the vector storing the vertices) to the vertex
+        DVertices.push_back(NewVertex); //Push the vertex to the vector
         return NewID;
     }
 
     std::any GetVertexTag(TVertexID id) const noexcept{
-        if(id < DVertices.size()){
-            return DVertices[id]->DTag;
+        if(id < DVertices.size()){ //Check if the ID is valid
+            return DVertices[id]->DTag; //Returns the tag of the vertex indexed by theID
         }
         return std::any();
     }
 
     bool AddEdge(TVertexID src, TVertexID dest, double weight, bool bidir = false) noexcept{
-        if(src < DVertices.size() && dest < DVertices.size()){
-            DVertices[src]->DEdges.push_back(std::make_pair(weight,DVertices[dest]));
+        if(src < DVertices.size() && dest < DVertices.size()){ //Checks if the IDs' are valid
+            DVertices[src]->DEdges.push_back(std::make_pair(weight,DVertices[dest])); //Create the edge
             if(bidir){
-                DVertices[dest]->DEdges.push_back(std::make_pair(weight,DVertices[src]));
+                DVertices[dest]->DEdges.push_back(std::make_pair(weight,DVertices[src])); //If bidirectional, create the edge going the other direction as well
             }
-            return true;
+            return true; //Return true on success
         }
         return false;
     }
@@ -51,17 +54,55 @@ struct CDijkstraPathRouter::SImplementation{
     }
 
     double FindShortestPath(TVertexID src, TVertexID dest, std::vector<TVertexID> &path) noexcept{
-        //std::vector<double> Weights;
-        //Weights.resize(DVertices.size(),std::numeric_limits<double>::max());
-        //std::vector<TVertexID> Previous;
-        //Previous.resize(DVertices.size(),std::numeric_limits<TVertexID>::max());
-        //path.clear();
+        path.clear();
+        std::vector<double> Weights; //Array to store the weight (distance) from the soruce node
+        Weights.resize(DVertices.size(),std::numeric_limits<double>::max()); //Initially, all nodes are infinitely away from the source node
+        std::vector<TVertexID> Previous; //Array to store the node preceding a given node, allowing us to construct the path by going backwards
+        Previous.resize(DVertices.size(),std::numeric_limits<TVertexID>::max()); //Initially, all nodes have no nodes preceding it as no path has been constructred it
+        std::priority_queue<std::pair<double,TVertexID>, std::vector<std::pair<double,TVertexID>>, std::greater<std::pair<double,TVertexID>>> pq; //Priority queue used to retrieve the (univisted) node with the shortest distance in O(log V)
 
-        // Push the exact sequence that the test expects
-        path = {0, 1, 3, 2};
+        Weights[src] = 0.0; //The distance from the source to itself is 0
+        pq.push(std::make_pair(0.0, src)); //Push the source and its weight to the queue to begin traversal
 
-        // Return the expected path length
-        return 7.0;
+        while(!pq.empty()){ //Loop until the heap is empty
+            auto node = pq.top(); //Pop the next univisted node with the shortest distance
+            pq.pop();
+            double edgeWeight = node.first;
+            TVertexID id = node.second;
+            if(edgeWeight > Weights[id]){  //Lazy Dijkstra: ignore entries in the queue that have longer distances than what is currently stored in the Weights[] array
+                continue;
+            }
+            
+            if(id == dest){ //If we find the destination node, break out of the loop early
+                break;
+            }
+
+            std::vector<TEdge> &adjacent = DVertices[id]->DEdges; //Iterates through the adjacency list
+            for(TEdge edge: adjacent){
+                auto weight = edge.first; //Distance from the node
+                auto neighbor = edge.second; //Shared pointer of the node, in which we have to convert to its ID
+                auto res = (std::find(DVertices.begin(), DVertices.end(), neighbor));
+                auto neighborid = static_cast<size_t>(std::distance(DVertices.begin(), res));
+
+                if (Weights[id] + weight < Weights[neighborid]){ //If we find a shorter distance from source to the node
+                    Weights[neighborid] = Weights[id] + weight; //Update weight
+                    Previous[neighborid] = id; //Update parent
+                    pq.push(std::make_pair(Weights[id] + weight, neighborid)); //Push the new entry into queue for evaluation of shortest distance and relaxation of adjacent nodes
+                }
+            }
+        }
+
+        if(Weights[dest] != std::numeric_limits<double>::max()){ //If a path from source to destination does exist, construct the path
+            TVertexID node = dest;
+            while(node != src){
+                path.push_back(node);
+                node = Previous[node];
+            }
+            path.push_back(src);
+            std::reverse(path.begin(), path.end()); //Reverse the the order of the vector
+        }
+
+        return Weights[dest]; //Return the final distance from source to destination
     }
 };
 
--- testsrc/DijkstraPathRouterTest.cpp
+++ testsrc/DijkstraPathRouterTest.cpp
@@ -9,7 +9,6 @@ TEST(DijkstraPathRouter, SimpleTest){
     EXPECT_EQ(PathRouter.VertexCount(),1);
     auto VetextTag = std::any_cast<std::string>(PathRouter.GetVertexTag(VertexID));
     EXPECT_EQ(VetextTag,"foo");
-    
 }
 
 TEST(DijkstraPathRouter, ShortestPath){
@@ -35,5 +34,78 @@ TEST(DijkstraPathRouter, ShortestPath){
     EXPECT_EQ(PathRouter.FindShortestPath(VertexA,VertexC,Path), 7.0);
     std::vector<CPathRouter::TVertexID> ExpectedPath{VertexA,VertexB,VertexD,VertexC};
     EXPECT_EQ(Path,ExpectedPath);
+}
+
+TEST(DijkstraPathRouter, ShortestPathHardTest){
+    CDijkstraPathRouter PathRouter;
+    auto A = PathRouter.AddVertex(std::string("A"));
+    auto B = PathRouter.AddVertex(std::string("B"));
+    auto C = PathRouter.AddVertex(std::string("C"));
+    auto D = PathRouter.AddVertex(std::string("D"));
+    auto E = PathRouter.AddVertex(std::string("E"));
+    auto F = PathRouter.AddVertex(std::string("F"));
+    auto G = PathRouter.AddVertex(std::string("G"));
+    auto H = PathRouter.AddVertex(std::string("H"));
+    EXPECT_EQ(PathRouter.VertexCount(),8);
+    EXPECT_TRUE(PathRouter.AddEdge(A,B,2.5));
+    EXPECT_TRUE(PathRouter.AddEdge(A,C,5.0));
+    EXPECT_TRUE(PathRouter.AddEdge(B,D,2.0));
+    EXPECT_TRUE(PathRouter.AddEdge(B,C,3.0));
+    EXPECT_TRUE(PathRouter.AddEdge(C,E,2.0));
+    EXPECT_TRUE(PathRouter.AddEdge(E,F,2.0));
+    EXPECT_TRUE(PathRouter.AddEdge(D,G,1.0));
+    EXPECT_TRUE(PathRouter.AddEdge(G,H,1.0));
+    EXPECT_TRUE(PathRouter.AddEdge(F,H,10.0));
+    EXPECT_TRUE(PathRouter.AddEdge(H,F,5.0));
+    std::vector<CPathRouter::TVertexID> Path;
+    EXPECT_EQ(PathRouter.FindShortestPath(A,H,Path),6.5);
+    std::vector<CPathRouter::TVertexID> ExpectedPath{A,B,D,G,H};
+    EXPECT_EQ(Path,ExpectedPath);
+    EXPECT_EQ(std::any_cast<std::string>(PathRouter.GetVertexTag(0)), "A");
+    EXPECT_EQ(std::any_cast<std::string>(PathRouter.GetVertexTag(7)), "H");
+    EXPECT_THROW(std::any_cast<std::string>(PathRouter.GetVertexTag(100)), std::bad_any_cast);
+}
+
+
+TEST(DijkstraPathRouter, ShortestPathUnreachableTest){
+    CDijkstraPathRouter PathRouter;
+    /*
+                5   
+       A ---> B --->C
+          4      ___^
+                / 1
+              D
     
+    */
+    auto VertexA = PathRouter.AddVertex(std::string("A"));
+    auto VertexB = PathRouter.AddVertex(std::string("B"));
+    auto VertexC = PathRouter.AddVertex(std::string("C"));
+    auto VertexD = PathRouter.AddVertex(std::string("D"));
+    EXPECT_EQ(PathRouter.VertexCount(),4);
+    EXPECT_TRUE(PathRouter.AddEdge(VertexA,VertexB,4.0));
+    EXPECT_TRUE(PathRouter.AddEdge(VertexB,VertexC,5.0));
+    EXPECT_TRUE(PathRouter.AddEdge(VertexD,VertexC,1.0));
+    std::vector<CPathRouter::TVertexID> Path;
+    EXPECT_EQ(PathRouter.FindShortestPath(VertexA,VertexD,Path), std::numeric_limits<double>::max());
+    std::vector<CPathRouter::TVertexID> ExpectedPath{};
+    EXPECT_EQ(Path,ExpectedPath);
+}
+
+TEST(DijkstraPathRouter, ShortestPathSelfTest){
+    CDijkstraPathRouter PathRouter;
+    /*  
+
+       A --
+       ^   | 9
+       | --
+
+    */
+    auto VertexA = PathRouter.AddVertex(std::string("A"));
+    EXPECT_EQ(PathRouter.VertexCount(),1);
+    EXPECT_TRUE(PathRouter.AddEdge(VertexA,VertexA,9.0));
+    std::vector<CPathRouter::TVertexID> Path;
+    EXPECT_EQ(PathRouter.FindShortestPath(VertexA,VertexA,Path), 0.0);
+    std::vector<CPathRouter::TVertexID> ExpectedPath{0};
+    EXPECT_EQ(Path,ExpectedPath);
+    EXPECT_TRUE(PathRouter.Precompute(std::chrono::steady_clock::now()));
 }
